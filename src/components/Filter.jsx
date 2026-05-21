adb194832e453087cc72b0795861af9ff8105b2d
Ronei.Ferreira <rifla2016@hotmail.com>
2026-05-21 18:22:13 -0300

    atualizei filter e soter

--- src/components/Filter.jsx
+++ src/components/Filter.jsx
@@ -1,11 +1,11 @@
-const Filter = () => {
+const Filter = ({ filter, setFilter, setSort }) => {
   return (
     <div className="filter">
         <h2>Filtrar:</h2>
         <div className="filter-options">
         <div>
-        <p>Statur:</p>
-        <select>
+        <p>Status:</p>
+        <select value={filter} onChange={(e) => setFilter(e.target.value)}>
             <option value="All">Todas</option>
             <option value="Completed">Completas</option>
             <option value="Incomplete">Incompletas</option>
@@ -14,8 +14,8 @@ const Filter = () => {
    
     <div>
         <p>Ordem alfabética</p>
-        <button>Asc</button>
-        <button>Desc</button>
+        <button onClick={() => setSort("Asc")}>Asc</button>
+        <button onClick={() => setSort("Desc")}>Desc</button>
     </div>
   </div>
 </div>
